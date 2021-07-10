e6c56f80b5d3bd0c9a649cb67fcf81029eb1da6f
mouradtlili <mourad.tlili@esprit.tn>
2021-07-10 17:40:41 +0100

    Finished Project

---
+++ src/Components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Button = ({color, text, onClick }) => {
+    return (
+       <button className='btn' 
+       onClick={onClick}
+       style={{backgroundColor: color }}>{ text }</button>
+
+    )
+}
+
+export default Button
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
